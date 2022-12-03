852f2248f6ab4013e3b5026846f3ce90a2e89670
Jakob Runge <github@runjak.codes>
2022-12-03 16:30:14 +0100

    remove stuff in an effor to restructure

--- assembly/01.ts
+++
@@ -1,3 +0,0 @@
-export function task_01_1(input: string): string {
-  return "test_" + input + "_this";
-}
--- tests/index.js
+++
@@ -1,5 +0,0 @@
-import assert from "assert";
-import { add, task_01_1 } from "../build/debug.js";
-assert.strictEqual(add(1, 2), 3);
-console.log(task_01_1("test"));
-console.log("ok");
