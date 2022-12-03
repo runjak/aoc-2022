42d92ebdad9b16afc608e8010209909434823c5f
Jakob Runge <github@runjak.codes>
2022-12-04 00:31:17 +0100

    add /test route to src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,5 @@
-//! Default Compute@Edge template program.
-
 use fastly::http::{header, Method, StatusCode};
 use fastly::{mime, Error, Request, Response};
-
-/// The entry point for your application.
-///
-/// This function is triggered when your service receives a client request. It could be used to
-/// route based on the request properties (such as method or path), send the request to a backend,
-/// make completely new requests, and/or generate synthetic responses.
-///
-/// If `main` returns an error, a 500 error response will be delivered to the client.
 
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
@@ -60,6 +50,10 @@ fn main(req: Request) -> Result<Response, Error> {
                 .with_content_type(mime::TEXT_HTML_UTF_8)
                 .with_body(include_str!("welcome-to-compute@edge.html")))
         }
+
+        "/test" => Ok(Response::from_status(StatusCode::OK)
+            .with_content_type(mime::TEXT_HTML_UTF_8)
+            .with_body("<html><body>Works?<br />Works :)!</body></html>")),
 
         // Catch all other requests and return a 404.
         _ => Ok(Response::from_status(StatusCode::NOT_FOUND)
