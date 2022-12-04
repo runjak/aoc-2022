ab0a2b330dd46c825ebe7d7714584d7e1dc93dc8
Jakob Runge <github@runjak.codes>
2022-12-04 22:29:08 +0100

    solve task 03 1

--- src/day_03.rs
+++ src/day_03.rs
@@ -1,6 +1,45 @@
+use std::collections::HashSet;
+
+fn common_item(rucksack: &str) -> Option<String> {
+    if rucksack.is_empty() {
+        return None;
+    }
+
+    let (left, right) = rucksack.split_at(rucksack.len() / 2);
+    let right_set: HashSet<_> = right.chars().collect();
+
+    return left
+        .chars()
+        .find(|item| right_set.contains(&item))
+        .map(|char| char.to_string());
+}
+
+fn item_priority(item: String) -> Option<i32> {
+    let small_a = "a".as_bytes().get(0)?;
+    let small_z = "z".as_bytes().get(0)?;
+    let big_a = "A".as_bytes().get(0)?;
+    let big_z = "Z".as_bytes().get(0)?;
+
+    let code = item.as_bytes().get(0)?;
+
+    if code >= small_a && code <= small_z {
+        return Some(i32::from(code - small_a + 1));
+    }
+
+    if code >= big_a && code <= big_z {
+        return Some(i32::from(code - big_a + 27));
+    }
+
+    return None;
+}
 
 pub fn task_03_1() -> String {
-    return String::from("to be implemented.");
+    let input = include_str!("../inputs/03/input.txt");
+    let common_items = input.split("\n").filter_map(common_item);
+
+    let priority_sum: i32 = common_items.filter_map(item_priority).sum();
+
+    return format!("{}", priority_sum);
 }
 
 pub fn task_03_2() -> String {
