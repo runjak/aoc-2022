c568b6352621de72f75bb6fd414baf2ac620f48c
Jakob Runge <github@runjak.codes>
2022-12-04 13:59:36 +0100

    solve task 01 1

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,24 @@ fn resp_index() -> Response {
         .with_body(include_str!("index.html"))
 }
 
+fn task_01_1() -> String {
+    let input = include_str!("../inputs/01/input.txt");
+    let elves = input.split("\n\n");
+
+    let mut max_calories: i32 = 0;
+
+    for elve in elves {
+        let inventory = elve.split("\n");
+        let calories = inventory.filter_map(|item| item.parse::<i32>().ok()).sum();
+
+        if max_calories < calories {
+            max_calories = calories;
+        }
+    }
+
+    return max_calories.to_string();
+}
+
 #[fastly::main]
 fn main(req: Request) -> Result<Response, Error> {
     match req.get_path() {
@@ -34,6 +52,12 @@ fn main(req: Request) -> Result<Response, Error> {
                 return Ok(resp_method_not_allowed());
             }
         },
+
+        "/01-1" => {
+            return Ok(Response::from_status(StatusCode::OK)
+                .with_content_type(mime::TEXT_PLAIN_UTF_8)
+                .with_body(task_01_1()));
+        }
 
         _ => Ok(Response::from_status(StatusCode::NOT_FOUND)
             .with_body_text_plain("The page you requested could not be found\n")),
