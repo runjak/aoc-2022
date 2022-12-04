4e5f6cc1af08c177fc3950fed3fbf615145b1110
Jakob Runge <github@runjak.codes>
2022-12-04 23:08:34 +0100

    solve task 03 2

--- src/day_03.rs
+++ src/day_03.rs
@@ -1,6 +1,6 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, slice::Chunks};
 
-fn common_item(rucksack: &str) -> Option<String> {
+fn duplicte_item(rucksack: &str) -> Option<String> {
     if rucksack.is_empty() {
         return None;
     }
@@ -35,13 +35,48 @@ fn item_priority(item: String) -> Option<i32> {
 
 pub fn task_03_1() -> String {
     let input = include_str!("../inputs/03/input.txt");
-    let common_items = input.split("\n").filter_map(common_item);
+    let duplicate_items = input.split("\n").filter_map(duplicte_item);
 
-    let priority_sum: i32 = common_items.filter_map(item_priority).sum();
+    let priority_sum: i32 = duplicate_items.filter_map(item_priority).sum();
 
     return format!("{}", priority_sum);
 }
 
+fn common_item(group: &[&str]) -> Option<String> {
+    if group.len() != 3 {
+        return None;
+    }
+
+    let mut group_iterator = group.into_iter();
+    let mut common_items: HashSet<_> = group_iterator.next()?.chars().collect();
+
+    for elve in group_iterator {
+        let mut intersecting_items: HashSet<char> = HashSet::new();
+
+        for item in elve.chars() {
+            if common_items.contains(&item) {
+                intersecting_items.insert(item);
+            }
+        }
+
+        common_items = intersecting_items;
+    }
+
+    if common_items.len() == 1 {
+        return common_items.into_iter().next().map(|item| item.to_string());
+    }
+
+    return None;
+}
+
 pub fn task_03_2() -> String {
-    return String::from("to be implemented.");
+    let input = include_str!("../inputs/03/input.txt");
+
+    let lines: Vec<_> = input.split("\n").collect();
+    let groups: Chunks<&str> = lines.chunks(3);
+
+    let common_items = groups.filter_map(common_item);
+    let priority_sum: i32 = common_items.filter_map(item_priority).sum();
+
+    return format!("{}", priority_sum);
 }
